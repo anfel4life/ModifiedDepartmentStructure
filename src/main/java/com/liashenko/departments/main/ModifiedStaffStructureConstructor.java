1d446f291796f59697b9cb7d912127e4dc7cdbbe
LiashenkoVitaliy <darkelf89@bigmir.net>
2017-03-08 16:01:47 +0200

    Added logger (Log4j)

--- src/main/java/com/liashenko/departments/main/ModifiedStaffStructureConstructor.java
+++ src/main/java/com/liashenko/departments/main/ModifiedStaffStructureConstructor.java
@@ -1,23 +1,24 @@
 package com.liashenko.departments.main;
 
 
-import com.liashenko.departments.services.mainDBService.MainServiceImpl;
-import com.liashenko.departments.services.mainService.MainService;
+import com.liashenko.departments.userInterface.CommandsParser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import com.liashenko.departments.userInterface.CommandsParser;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 public class ModifiedStaffStructureConstructor {
-    static final Logger logger = LogManager.getLogger(ModifiedStaffStructureConstructor.class.getName());
+//    static final Logger logger = LogManager.getLogger(ModifiedStaffStructureConstructor.class.getName());
+    static final Logger rootLogger = LogManager.getRootLogger();
+    static final Logger classLogger = LogManager.getLogger(ModifiedStaffStructureConstructor.class);
 
     public static void main(String[] args) {
         BufferedReader br = null;
         boolean isContinue = true;
-        logger.info("Logger run");
+        classLogger.info("Logger run");
 //        if (ConnectionUtils.isTablesExist()) {
             CommandsParser comParser = new CommandsParser();
             System.out.print("Enter command and press <Enter> (\"help\" for help).");
--- src/main/java/com/liashenko/departments/services/mainDBService/MainServiceImpl.java
+++ src/main/java/com/liashenko/departments/services/mainDBService/MainServiceImpl.java
@@ -41,23 +41,11 @@ public class MainServiceImpl implements MainService {
         configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/STAFF");
         configuration.setProperty("hibernate.connection.username", "root");
         configuration.setProperty("hibernate.connection.password", "1");
+        configuration.setProperty("hibernate.connection.autocommit", "true");
         configuration.setProperty("useSSL", "false");
         configuration.setProperty("hibernate.show_sql", hibernate_show_sql);
         configuration.setProperty("hibernate.hbm2ddl.auto", hibernate_hbm2ddl_auto);
         return configuration;
-    }
-
-    public void printConnectInfo() {
-        try {
-            SessionFactoryImpl sessionFactoryImpl = (SessionFactoryImpl) sessionFactory;
-            Connection connection = sessionFactoryImpl.getConnectionProvider().getConnection();
-            System.out.println("DB name: " + connection.getMetaData().getDatabaseProductName());
-            System.out.println("DB version: " + connection.getMetaData().getDatabaseProductVersion());
-            System.out.println("Driver: " + connection.getMetaData().getDriverName());
-            System.out.println("Autocommit: " + connection.getAutoCommit());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     private static SessionFactory createSessionFactory(Configuration configuration) {
